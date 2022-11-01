882b1a24e5a270d247b48db58b536f81159e3ab4
samandarrza <samandarshr@code.edu.az>
2022-11-02 02:14:15 +0400

    add

---
+++ Algoritm/assets/js/main.js
@@ -0,0 +1,8 @@
+const arr1 = [3, 5, 6, 10, 11, 20];
+const arr2 = [1, 2, 7, 8, 15, 19];
+
+function marge(arr1, arr2,...rest) {
+    const result = [...arr1, ...arr2,...rest]
+    return result.sort((arr1, arr2)=> arr1 - arr2);
+}
+console.log(marge(arr1,arr2,[44,45,46],[46,48,40]));
